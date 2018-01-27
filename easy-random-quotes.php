10cef6b143827bb8acdda306ac8dee0359180d10
Kailey Lampert <trepmal@gmail.com>
2018-01-27 12:03:19 -0800

    assorted formatting, escaping, cleaning

--- easy-random-quotes.php
+++ easy-random-quotes.php
@@ -47,11 +47,11 @@ class Easy_Random_Quotes {
 			__( 'Easy Random Quotes', 'easy-random-quotes' ),
 			__( 'Easy Random Quotes', 'easy-random-quotes' ),
 			'manage_options',
-			__FILE__,
+			__CLASS__,
 			array( $this, 'page' )
 		);
 
-		add_action( 'admin_head-'.$this->page_name, array( $this, 'verify_nonce' ) );
+		add_action( 'admin_head-' . $this->page_name, array( $this, 'verify_nonce' ) );
 
 	}
 
@@ -81,10 +81,10 @@ class Easy_Random_Quotes {
 
 				update_option( 'kl-easyrandomquotes', $theQuotes ); //successfully updated
 
-				$this->alert_message( __( 'New quote was added' , 'easy-random-quotes' ) );
+				$this->alert_message( __( 'New quote was added', 'easy-random-quotes' ) );
 
 			} else {
-				$this->alert_message( __( 'Nothing added' , 'easy-random-quotes' ) );
+				$this->alert_message( __( 'Nothing added', 'easy-random-quotes' ) );
 			}
 
 		} // end if add
@@ -130,9 +130,9 @@ class Easy_Random_Quotes {
 			check_admin_referer( 'easyrandomquotes-update_edit' );
 
 			if ( update_option( 'kl-easyrandomquotes',$theQuotes ) ) {
-				$this->alert_message( __( 'Quote was edited/deleted' , 'easy-random-quotes' ) );
+				$this->alert_message( __( 'Quote was edited/deleted', 'easy-random-quotes' ) );
 			} else {
-				$this->alert_message( __( 'Nothing changed' , 'easy-random-quotes' ) );
+				$this->alert_message( __( 'Nothing changed', 'easy-random-quotes' ) );
 			}
 
 		} // end if edit
@@ -141,11 +141,11 @@ class Easy_Random_Quotes {
 
 			if ( ! isset( $_POST[ 'confirm' ] ) ) {
 
-				$this->alert_message( __( 'You must confirm for a reset' , 'easy-random-quotes' ) );
+				$this->alert_message( __( 'You must confirm for a reset', 'easy-random-quotes' ) );
 
 			} elseif ( delete_option( 'kl-easyrandomquotes' ) ) {
 
-				$this->alert_message( __( 'All quotes deleted' , 'easy-random-quotes' ) );
+				$this->alert_message( __( 'All quotes deleted', 'easy-random-quotes' ) );
 
 			}
 
@@ -155,7 +155,11 @@ class Easy_Random_Quotes {
 
 
 	function alert_message( $message, $type='updated' ) {
-		echo '<div class="' . esc_attr( $type ) . '"><p>' . $message . '</p></div>';
+		printf(
+			'<div class="%s"><p>%s</p></div>',
+			esc_attr( $type ),
+			esc_html( $message )
+		);
 	}
 
 	/**
@@ -165,52 +169,54 @@ class Easy_Random_Quotes {
 		$this->update();
 		echo '<div class="wrap">';
 
-		echo '<h2>' . __( 'Easy Random Quotes' , 'easy-random-quotes' ) . '</h2>';
+		echo '<h2>' . esc_html__( 'Easy Random Quotes', 'easy-random-quotes' ) . '</h2>';
 
-		echo '<h3>' . __( 'Add Quote' , 'easy-random-quotes' ) . '</h3>';
+		echo '<h3>' . esc_html__( 'Add Quote', 'easy-random-quotes' ) . '</h3>';
+
 			echo '<form method="post">';
+
 			wp_nonce_field( 'easyrandomquotes-update_add' );
-			echo '<table class="widefat page"><thead><tr><th class="manage-column" colspan = "2">' . __( 'Add New' , 'easy-random-quotes' ) . '</th></tr></thead><tbody><tr>';
+			echo '<table class="widefat page"><thead><tr><th class="manage-column" colspan = "2">' . esc_html__( 'Add New', 'easy-random-quotes' ) . '</th></tr></thead><tbody><tr>';
 			echo '<td><textarea name="erq_newquote" rows="6" cols="60"></textarea></td>';
-			echo '<td><p>';
 
-			submit_button( __( 'Add' , 'easy-random-quotes' ), 'small', 'erq_add', false );
-			echo ' ';
-			submit_button( __( 'Import' , 'easy-random-quotes' ), 'small', 'erq_import', false );
+			printf(
+				"<td><p>%s %s</p><p>%s</p></td>",
+				get_submit_button( esc_html__( 'Add', 'easy-random-quotes' ), 'small', 'erq_add', false ),
+				get_submit_button( esc_html__( 'Import', 'easy-random-quotes' ), 'small', 'erq_import', false ),
+				esc_html__( 'With Import, each new line will be treated as the start of a new quote', 'easy-random-quotes' )
+			);
 
-			echo '</p><p>'. __( 'With Import, each new line will be treated as the start of a new quote', 'easy-random-quotes' ) .'</p></td>';
-			echo '</tr></tbody></table>';
-			echo '</form>';
+			echo '</tr></tbody></table></form>';
 
-		echo '<h3>' . __( 'Edit Quotes' ) . '</h3>';
+		echo '<h3>' . esc_html__( 'Edit Quotes' ) . '</h3>';
 			echo '<form method="post">';
 			wp_nonce_field( 'easyrandomquotes-update_edit' );
 
-			$tblrows = '<tr><td class="manage-column column-cb check-column" id="cb"><input type="checkbox" /></th>
-							<th scope="col" id="quote" class="manage-column column-quote">' . __( 'The quote' , 'easy-random-quotes' ) . '</th>
-							<th scope="col" id="shortcode" class="manage-column column-shortcode">' . __( 'Short code (for posts/pages)' , 'easy-random-quotes' ) . '</th></tr>';
+			$escaped_tblrows = '<tr><td class="manage-column column-cb check-column" id="cb"><input type="checkbox" /></th>
+							<th scope="col" id="quote" class="manage-column column-quote">' . esc_html__( 'The quote', 'easy-random-quotes' ) . '</th>
+							<th scope="col" id="shortcode" class="manage-column column-shortcode">' . esc_html__( 'Short code (for posts/pages)', 'easy-random-quotes' ) . '</th></tr>';
 
-			echo '<table class="widefat page"><thead>' . $tblrows . '</thead><tfoot>' . $tblrows . '</tfoot><tbody>';
+			echo '<table class="widefat page"><thead>' . $escaped_tblrows . '</thead><tfoot>' . $escaped_tblrows . '</tfoot><tbody>';
 
-			$theQuotes =  get_option( 'kl-easyrandomquotes', array() ) ;
+			$all_quotes =  get_option( 'kl-easyrandomquotes', array() ) ;
 
-			if ( ! empty( $theQuotes ) ) {
-				foreach ( $theQuotes as $id=>$quote ) {
+			if ( ! empty( $all_quotes ) ) {
+				foreach ( $all_quotes as $id => $quote ) {
 					echo '<tr>';
-					echo '<th class="check-column"><input type="checkbox" name="erq_del[' . $id . ']" /></th>';
-					echo '<td><textarea name="erq_quote[' . $id . ']" rows="6" cols="60">' . esc_textarea( stripslashes( $quote ) ) . '</textarea></td>';
-					echo '<td>[erq id=' . $id . ']</td>';
+					echo '<th class="check-column"><input type="checkbox" name="erq_del[' . esc_attr( $id ) . ']" /></th>';
+					echo '<td><textarea name="erq_quote[' . esc_attr( $id ) . ']" rows="6" cols="60">' . esc_textarea( stripslashes( $quote ) ) . '</textarea></td>';
+					echo '<td>[erq id=' . esc_attr( $id ) . ']</td>';
 					echo '</tr>';
 				}
 			} else {
-				echo '<tr><th colspan="3">' . __( 'No quotes' , 'easy-random-quotes' ) . '</th></tr>';
+				echo '<tr><th colspan="3">' . esc_html__( 'No quotes', 'easy-random-quotes' ) . '</th></tr>';
 			}
 
 			echo '</tbody></table>';
 
 			echo '<p>';
-			submit_button( __( 'Save Changes' , 'easy-random-quotes' ), 'primary', 'submit', false );
-			echo ' <span class="description">' . __( 'Checked items will be deleted' , 'easy-random-quotes' ) . '</span>';
+			submit_button( esc_html__( 'Save Changes', 'easy-random-quotes' ), 'primary', 'submit', false );
+			echo ' <span class="description">' . esc_html__( 'Checked items will be deleted', 'easy-random-quotes' ) . '</span>';
 			echo '</p>';
 			echo '</form>';
 
@@ -225,19 +231,35 @@ class Easy_Random_Quotes {
 
 		if ( $screen_id != $this->page_name ) return;
 
-		$string1 = sprintf( __( 'Specific quote: %s' , 'easy-random-quotes' ), '<code>[erq id=2]</code>' );
-		$string2 = sprintf( __( 'Random quote: %s' , 'easy-random-quotes' ), '<code>[erq]</code>' );
-		$content = '<p><strong>' . __( 'Shortcode' , 'easy-random-quotes' ) . '</strong><br />'. $string1 .'<br />' . $string2 .'</p>';
+		$eg_specific = sprintf( esc_html__( 'Specific quote: %s', 'easy-random-quotes' ), '<code>[erq id=2]</code>' );
+		$eg_random   = sprintf( esc_html__( 'Random quote: %s', 'easy-random-quotes' ), '<code>[erq]</code>' );
+		$content = sprintf(
+			'<p><strong>%s</strong><br />%s<br />%s</p>',
+			esc_html__( 'Shortcode', 'easy-random-quotes' ),
+			$eg_specific,
+			$eg_random
+		);
 
-		$string1 = sprintf( __( 'Specific quote: %s' , 'easy-random-quotes' ), '<code>' . htmlspecialchars( '<?php echo erq_shortcode(array(\'id\' => \'2\')); ?>' ) . '</code>' );
-		$string2 = sprintf( __( 'Random quote: %s' , 'easy-random-quotes' ), '<code>' . htmlspecialchars( '<?php echo erq_shortcode(); ?>' ) . '</code>' );
-		$content .= '<p><strong>' . __( 'Template tag' , 'easy-random-quotes' ) . '</strong><br />'. $string1 .'<br />' . $string2 .'</p>';
+		$eg_specific = sprintf( esc_html__( 'Specific quote: %s', 'easy-random-quotes' ), '<code>' . htmlspecialchars( '<?php echo erq_shortcode(array(\'id\' => \'2\')); ?>' ) . '</code>' );
+		$eg_random   = sprintf( __( 'Random quote: %s', 'easy-random-quotes' ), '<code>' . htmlspecialchars( '<?php echo erq_shortcode(); ?>' ) . '</code>' );
+		$content .= sprintf(
+			'<p><strong>%s</strong><br />%s<br />%s</p>',
+			esc_html__( 'Template tag', 'easy-random-quotes' ),
+			$eg_specific,
+			$eg_random
+		);
 
-		$content .= '<p>' . __( 'Quotes retained when plugin deactivated. Quotes deleted when plugin removed.' , 'easy-random-quotes' ) . '</p>';
+		$content .= sprintf(
+			'<p>%s</p>',
+			esc_html__( 'Quotes retained when plugin deactivated. Quotes deleted when plugin removed.', 'easy-random-quotes' )
+		);
 
-		$content .= '<form method="post">';
-		$content .= get_submit_button( __( 'Delete All Quotes' , 'easy-random-quotes' ), 'secondary', 'clear', false ) .
-		' <label><input type="checkbox" name="confirm" value="true" />' . __( 'Check to confirm' , 'easy-random-quotes' ) . '</label></form>';
+		$content .= sprintf(
+			'<form method="post">%s <label><input type="checkbox" name="confirm" value="true" />%s</label></form>',
+			get_submit_button( esc_html__( 'Delete All Quotes', 'easy-random-quotes' ), 'secondary', 'clear', false ),
+			esc_html__( 'Check to confirm', 'easy-random-quotes' )
+		);
+
 
 		$screen->add_help_tab( array(
 			'id'      => 'erq_help',
@@ -264,10 +286,10 @@ function erq_shortcode( $atts=array() ) {
 	shuffle( $id );
 	$id = array_pop( $id );
 
-	$theQuotes = get_option( 'kl-easyrandomquotes', array() ); //get exsisting
-	$use = ( 'rand' == $id ) ? array_rand( $theQuotes ) : $id;
-	if ( isset( $theQuotes[ $use ] ) ) {
-		return stripslashes( $theQuotes[ $use ] );
+	$all_quotes = get_option( 'kl-easyrandomquotes', array() ); //get exsisting
+	$use = ( 'rand' === $id ) ? array_rand( $all_quotes ) : $id;
+	if ( isset( $all_quotes[ $use ] ) ) {
+		return stripslashes( $all_quotes[ $use ] );
 	}
 }
 
@@ -289,13 +311,23 @@ class Easy_Random_Quotes_Widget extends WP_Widget {
 	 *
 	 */
 	function widget( $args, $instance ) {
-		extract( $args );
-		echo $before_widget;
+
+		$before_widget = $args['before_widget'];
+		$after_widget = $args['after_widget'];
+		$before_title = $args['before_title'];
+		$after_widget = $args['after_widget'];
+
+		$output = '';
+		$output .= $before_widget;
+
 		if ( ! $instance[ 'hide_title' ] ) {
-			echo $before_title . apply_filters( 'widget_title', $instance[ 'title' ] ) . $after_title;
+			$output .= $before_title . apply_filters( 'widget_title', $instance[ 'title' ] ) . $after_title;
 		}
-		echo '<p>' . erq_shortcode() . '</p>';
-		echo $after_widget;
+
+		$output .= '<p>' . wp_kses_post( erq_shortcode() ) . '</p>';
+		$output .= $after_widget;
+
+		echo wp_kses_post( $output );
 	}
 
 	/**
@@ -319,13 +351,13 @@ class Easy_Random_Quotes_Widget extends WP_Widget {
 		$instance = wp_parse_args( (array) $instance, $defaults );
 		?>
 		<p>
-			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'easy-random-quotes' );?>
-				<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $instance['title']; ?>" />
+			<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_html_e( 'Title:', 'easy-random-quotes' );?>
+				<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name('title') ); ?>" type="text" value="<?php echo esc_attr( $instance['title'] ); ?>" />
 			</label>
 		</p>
 		<p>
-			<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id('hide_title'); ?>" name="<?php echo $this->get_field_name('hide_title'); ?>"<?php checked( $instance['hide_title'] ); ?> />
-			<label for="<?php echo $this->get_field_id('hide_title'); ?>"><?php _e('Hide Title?', 'easy-random-quotes' );?></label>
+			<input type="checkbox" class="checkbox" id="<?php echo esc_attr( $this->get_field_id('hide_title') ); ?>" name="<?php echo esc_attr( $this->get_field_name('hide_title') ); ?>"<?php checked( $instance['hide_title'] ); ?> />
+			<label for="<?php echo esc_attr( $this->get_field_id( 'hide_title' ) ); ?>"><?php esc_html_e('Hide Title?', 'easy-random-quotes' );?></label>
 		</p>
 		<?php
 	}
